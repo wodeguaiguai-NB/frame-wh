1007da679d648d52ce00fb0d49bc4ef8ee80631c
mianhuatangtwo <mianhuatangonly@163.com>
2017-10-18 18:50:45 +0800

    第一次提交memo

---
+++ src/main/java/com/team/framenb/FrameNbApplication.java
@@ -0,0 +1,12 @@
+package com.team.framenb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameNbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameNbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/team/framenb/FrameNbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.team.framenb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrameNbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
