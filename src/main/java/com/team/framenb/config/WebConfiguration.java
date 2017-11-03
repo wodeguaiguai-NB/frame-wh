4e378d42266d4cfd08e3f0aef07888c96d18f292
haohao <1041840683@qq.com>
2017-11-03 23:43:13 +0800

    添加actuator

--- src/main/java/com/team/framenb/config/WebConfiguration.java
+++ src/main/java/com/team/framenb/config/WebConfiguration.java
@@ -1,18 +1,9 @@
 package com.team.framenb.config;
 
-import com.team.framenb.interceptor.RequestInterceptor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 /**
  * @author haohao
  * @create 2017/10/22 下午10:19
  **/
-public class WebConfiguration extends WebMvcConfigurerAdapter {
+public class WebConfiguration  {
 
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new RequestInterceptor()).addPathPatterns("/*");
-    }
 }
