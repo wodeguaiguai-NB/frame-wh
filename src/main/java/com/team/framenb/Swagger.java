f020af45b0c53e13c955e7389a25fc7fdcde528c
haohao <1041840683@qq.com>
2017-11-25 15:29:55 +0800

    添加Swagger2组件用于构建RESTFul Api文档

---
+++ src/main/java/com/team/framenb/Swagger.java
@@ -0,0 +1,39 @@
+package com.team.framenb;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger2配置类
+ *
+ * @author haohao
+ * @create 2017/11/25 下午2:55
+ **/
+@Configuration
+@EnableSwagger2
+public class Swagger {
+
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                        .apiInfo(apiInfo())
+                        .select()
+                        .apis(RequestHandlerSelectors.basePackage("com.team.framenb.controller"))
+                        .paths(PathSelectors.any())
+                        .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                        .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                        .description("frame-nb RESTApi")
+                        .contact("我得怪怪")
+                        .version("1.0")
+                        .build();
+    }
+}
--- src/main/java/com/team/framenb/controller/TestController.java
+++ src/main/java/com/team/framenb/controller/TestController.java
@@ -4,6 +4,7 @@ import com.google.gson.GsonBuilder;
 import com.team.framenb.entity.User;
 import com.team.framenb.service.UserService;
 import com.team.framenb.vo.UserVo;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +29,7 @@ public class TestController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "测试接口",notes = "测试restful接口")
     @GetMapping("/hello/{name}")
     public void hello(@PathVariable String name, HttpServletRequest request, HttpServletResponse response) throws Exception{
 
@@ -38,6 +40,7 @@ public class TestController {
         response.getWriter().write("Hello "+name+"!");
     }
 
+    @ApiOperation(value = "添加人员",notes = "添加人员信息")
     @PostMapping("/user/add")
     public void add(@RequestBody UserVo userVo, HttpServletResponse response) throws Exception{
         User user = this.userService.addUser(userVo.getName(),userVo.getAge());
@@ -47,12 +50,14 @@ public class TestController {
     }
 
 
+    @ApiOperation(value = "查找人员",notes = "根据url中的ID获取人员信息")
     @GetMapping("/user/find/{id}")
     public void findUserByName(@PathVariable Integer id ,HttpServletRequest request, HttpServletResponse response) throws Exception{
         User user = this.userService.findUserById(id);
         response.getWriter().write(new GsonBuilder().create().toJson(user));
     }
 
+    @ApiOperation(value = "获取人员列表", notes = "根据url中的name查找人员列表")
     @GetMapping("/user/findAll/{name}")
     public void findAllUserByName(@PathVariable String name ,HttpServletRequest request, HttpServletResponse response) throws Exception{
         List<User> users = this.userService.findAllUserByName(name);
