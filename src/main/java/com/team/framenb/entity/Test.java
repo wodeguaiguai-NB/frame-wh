87b09bd8a905a5a75b355c8eb060e94ecd26e6cf
haohao <1041840683@qq.com>
2018-01-10 22:34:48 +0800

    添加docker组件以及DOCKERfile

---
+++ src/main/java/com/team/framenb/entity/Test.java
@@ -0,0 +1,18 @@
+package com.team.framenb.entity;
+
+/**
+ * @author haohao
+ * @create 2018/01/09 01:29
+ **/
+public class Test {
+
+    private String name ;
+
+
+    private String gender;
+
+
+
+
+
+}
